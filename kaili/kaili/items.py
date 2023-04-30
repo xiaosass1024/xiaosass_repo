3286d92126d4624bb177e898584306aad0ba794b
xiaosass <xiaosass@xiaosassdeMacBook-Air.local>
2023-04-30 17:42:07 +0800

    add scrapy

---
+++ kaili/kaili/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class KailiItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    catalogAllName = scrapy.Field()
+    commodityName = scrapy.Field()
+    marketPrice = scrapy.Field()
+    salePrice = scrapy.Field()
+    supplierName = scrapy.Field()
+    pass
---
+++ kaili/kaili/spiders/kaili_spider.py
@@ -0,0 +1,48 @@
+import json
+
+import scrapy
+
+from ..items import KailiItem
+
+
+class KailiSpiderSpider(scrapy.Spider):
+    name = "kaili_spider"
+    allowed_domains = ["cnncmall.com"]
+    # start_urls = ["http://cnncmall.com/"]
+    url = 'https://www.cnncmall.com/cnnc/mall/noauth/searchGoods'
+    headers = {
+        'Cookie': 'auth-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjU5MDUwOTkwMTg1MTIwNTY0NywiaWF0IjoxNjgyODM1NTg0MDk5LCJsb2dpblNvdXJjZSI6InBjLXdlYiJ9.T-u-o7tpMKNtndJfqI2zFNHBk07Y37UhZyiRKOEngGA',
+        'Host': 'www.cnncmall.com',
+        'Origin': 'https://www.cnncmall.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+    }
+
+    def start_requests(self):
+        data = {"queryStr": "", "queryChannelId": "1008", "categoryId": "", "catalogName": "", "level": "",
+                "brandId": "",
+                "supplierShopId": "", "orderByColumn": "", "orderType": 0, "queryParams": [
+                {"filterId": "vendor_id_name", "filterName": "供应商", "filterValues": ["京东（员工活动）"],
+                 "categoryId": ""}], "agreementId": "", "minSalesPrice": 0, "maxSalesPrice": "",
+                "orgPath": "1-305775845729763327-400000010000062520-400000010000062521-400000010000062556-",
+                "userId": "590509901851205647", "pageSize": 10, "pageNo": 1, "activityId": "843819168674254848",
+                "doContract": "false", "doActity": "true", "doSelect": 1, "commodityTypeIds": [], "havStock": 0,
+                "qryToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjU5MDUwOTkwMTg1MTIwNTY0NywiaWF0IjoxNjgyNDI4NjA2NTE5LCJsb2dpblNvdXJjZSI6InBjLXdlYiJ9.KNKoB9Qp9ORGoej6qdFKWQUeKogadJUtrtTw0gZlph8",
+                "rsGoodsSearch": "false", "psDiscountRate": 0, "doMallContract": "true", "memUserType": "4", "supId": 0,
+                "outerUserTypes": ["4"], "province": "15", "city": "1243", "county": "1248", "town": "54971"}
+
+        yield scrapy.Request(url=self.url, body=json.dumps(data), method='POST', headers=self.headers,
+                             callback=self.parse_list)
+
+    def parse_list(self, response):
+        dict_data = response.json()
+        results = dict_data['data']['result']
+        for result in results:
+            catalogAllName = result['catalogAllName']
+            commodityName = result['commodityName']
+            marketPrice = result['marketPrice']
+            salePrice = result['salePrice']
+            supplierName = result['supplierName']
+            # print(KailiItem(catalogAllName=catalogAllName, commodityName=commodityName, marketPrice=marketPrice,
+            #                 salePrice=salePrice, supplierName=supplierName))
+            print(catalogAllName, commodityName, marketPrice, salePrice, supplierName)
+        pass
