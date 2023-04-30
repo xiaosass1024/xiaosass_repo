f4568feb8a1518ff3228514e5233d17de428d25e
xiaosass <xiaosass@xiaosassdeMacBook-Air.local>
2023-04-30 19:30:59 +0800

    完善pipline实现自定义表头

--- kaili/kaili/pipelines.py
+++ kaili/kaili/pipelines.py
@@ -13,14 +13,16 @@ class CSVKailiPipeline:
     def open_spider(self, spider):
         self.f = open('京东.csv', mode='a', encoding='utf-8', newline='')
         self.csv_writer = csv.DictWriter(self.f,
-                                         fieldnames=['catalogAllName', 'commodityName', 'marketPrice', 'salePrice',
-                                                     'supplierName'])
+                                         fieldnames=['品类', '品名', '市价', '折价',
+                                                     '商家'])
         self.csv_writer.writeheader()
 
     def process_item(self, item, spider):
         d = dict(item)
-        print(d)
-        self.csv_writer.writerow(d)
+        # print(d)
+        self.csv_writer.writerow(
+            {'品类': d['catalogAllName'], '品名': d['commodityName'], '市价': d['marketPrice'], '折价': d['salePrice'],
+             '商家': d['supplierName'], })
         return item
 
     def close_spider(self, spider):
